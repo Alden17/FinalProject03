917aeddc434683cbbb09d2f09a1a3021be6bd4c9
Alden17 <aldendzakwan97@gmail.com>
2025-10-01 13:29:04 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama");
-        System.out.println("Hi, ini baris kode Kedua");
+        System.out.println("Hi, ini baris pertama oleh user 2");
+        System.out.println("User 2 menambahkan baris baru");
     }
 }
