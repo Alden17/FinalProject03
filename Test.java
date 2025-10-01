f6a3dfc03ef35753aa92fc104abe85f6ece35f86
Alden <aldendzakwan@gmail.com>
2025-10-01 13:30:53 +0700

    edit user 1

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama");
+        System.out.println("Hi, ini baris kode pertama yang diubah oleh user 1");
+        System.out.println("User 1 menambahkan baris baru");
     }
 }
